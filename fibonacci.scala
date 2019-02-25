9f99e1acb7d3ff142c8dee02ce51cf8900c2be4c
nikhil kishore <kishore5790@gmail.com>
2019-02-26 00:20:15 +1300

    changes

--- fibonacci.scala
+++ fibonacci.scala
@@ -1,7 +1,10 @@
-def fibonacci(n: Int): Int = {
-  @annotation.tailrec
-  def go(prev_val: Int, curr_val: Int, iter: Int, maxiter: Int): Int =
-    if (iter == maxiter) curr_val
-    else go(curr_val, curr_val + prev_val, iter+1, maxiter)
+object mod {
+  def fibonacci(n: Int): Int = {
+    @annotation.tailrec
+    def go(prev_val: Int, curr_val: Int, iter: Int, maxiter: Int): Int = {
+      if (iter == maxiter) curr_val
+      else go(curr_val, curr_val + prev_val, iter+1, maxiter)
+    }
     go(0, 1, 2, n)
+  }
 }
